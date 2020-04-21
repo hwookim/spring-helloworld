f637f0d2beace44b71b7477dc495494daf45921a
hwookim <gusdn9754@gmail.com>
2020-04-21 11:03:01 +0900

    feat: 컨트롤러에 repository 적용

--- src/main/java/wooteco/helloworld/HelloController.java
+++ src/main/java/wooteco/helloworld/HelloController.java
@@ -1,14 +1,23 @@
 package wooteco.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private HelloRepository helloRepository;
 
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "") String name) {
-        return "Hello " + name;
+        if (name.isEmpty()) {
+            return "HelloWorld!";
+        }
+
+        helloRepository.save(new Hello(name));
+        int count = helloRepository.countByName(name);
+        return "Hello " + name + " " + count + "번째 방문입니다.";
     }
 }
